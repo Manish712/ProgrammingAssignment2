ef8a0bcfe1612a746b7e30ab84863063985231ef
Manish Kumar <64950588+Manish712@users.noreply.github.com>
2020-05-15 17:33:43 +0530

    Update cachematrix.R
    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The following two functions can be used to create a matrix and calculate its 
+#inverse with the ability to cache the inverse matrix if the original matrix remains
+#unchanged.
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  
+  #set() function enables the user to enter a valid matrix
+  set <- function(y) {
+    if(class(y)!="matrix")
+    {
+      message("Input is not a matrix! Please enter a valid matrix")
+    }
+    else
+    {
+      x <<- y
+      i <<- NULL
+    }
+  }
+  
+  #get() function enables the user to retrieve the value of  matrix
+  get <- function() x
+  
+  #setinverse() function is used by the cachesolve() function to cache the value of the inverse matrix
+  setinverse <- function(inverse) i <<- inverse
+  
+  #getinverse() function is used by the cachesolve() function to retrieve the value of the cached inverse matrix
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+
 
 }
 
@@ -12,4 +41,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  #Inverse of the matrix stored using makeCacheMatrix() is calculated        
+  i <- x$getinverse()
+  
+  #condition to check whether the original matrix has been modified
+  if(!is.null(i)) {
+    message("getting cached data")
+    
+    #CONDITION IS TRUE: the cached value of the inverse matrix is returned
+    return(i)
+  }
+  
+  data <- x$get()
+  
+  #CONDITION IS FALSE: inverse of the given matrix is calulated anew
+  i <- solve(data, ...)
+  x$setinverse(i)
+  
+  #the inverse matrix is returned
+  i
 }
